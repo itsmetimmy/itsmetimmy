0783a3529ce70d6e2afa1d9356ac9b1383734aba
Juz Timmy <77535408+itsmetimmy@users.noreply.github.com>
2021-11-12 20:33:34 +0630

    Update README.py

--- README.py
+++ README.py
@@ -1,9 +1,15 @@
 
-timmy = {"Age": "17", "Pron": "He/him", "Ptype": "INFP-T"}
+timmy = {"name": "Timmy",
+         "Age": "17", 
+         "Pron": "He/him", 
+         "Ptype": "INFP-T",
+         "Likes": "Programming",
+         "dislikes": "Syntax Errors"}
+
 for x in timmy:
     print(f"{x} : {timmy[x]}")
 
-<!---
-itsmetimmy/itsmetimmy is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+
+#itsmetimmy/itsmetimmy is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+#You can click the Preview link to take a look at your changes.
+
